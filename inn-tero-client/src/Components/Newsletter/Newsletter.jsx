4fcbbf42aa524a02246693c41f55d74bb77cda21
Sahil Abrar <zeroxkhan2@gmail.com>
2023-11-05 19:30:26 +0600

    Added newsletter form

--- inn-tero-client/src/Components/Newsletter/Newsletter.jsx
+++ inn-tero-client/src/Components/Newsletter/Newsletter.jsx
@@ -1,7 +1,55 @@
 const Newsletter = () => {
   return (
     <div>
-      <h1>Newsletter</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="flex gap-10">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered"
+                    required
+                    name="name"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                    name="email"
+                  />
+                </div>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Description"
+                  className="input input-bordered"
+                  required
+                  name="description"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
