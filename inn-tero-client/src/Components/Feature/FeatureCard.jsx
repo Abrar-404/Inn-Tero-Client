eb2b436c7d52318204b84d63cfcba71977a5b465
Sahil Abrar <zeroxkhan2@gmail.com>
2023-11-07 16:50:47 +0600

    added environment config

--- inn-tero-client/src/Components/Feature/FeatureCard.jsx
+++ inn-tero-client/src/Components/Feature/FeatureCard.jsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom';
-import '../Styles/card.css';
+// import '../Styles/card.css';
+import '../Styles/feature.css';
 
 const FeatureCard = ({ feature }) => {
   const { img, description } = feature || {};
   return (
     <div>
-      <div className="card scale-in-center shadow-xl ">
+      <div className="card scale-in-center  shadow-xl ">
         <figure>
           <img
-            className="w-[100%] scale-in-center h-[300px]"
+            className="w-[100%] roll-in-blurred-right h-[300px]"
             src={img}
             alt="Shoes"
           />
         </figure>
-        <div className="card-body ">
+        <div className="card-body transform scale-100 hover:scale-110 transition-transform ">
           <h2 className="card-title text-xl ">
             {description}
             <div className="badge badge-secondary">Featured</div>
