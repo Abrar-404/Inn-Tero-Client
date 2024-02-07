e051c3b81b8818beda0ce87aa7c6f7d72c224c0d
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-07 22:54:09 +0600

    added stat functions

--- inn-tero-client/src/Components/Stat/Stat.jsx
+++ inn-tero-client/src/Components/Stat/Stat.jsx
@@ -5,15 +5,14 @@ import { Link } from 'react-router-dom';
 const Stat = () => {
   return (
     <div className="flex justify-center">
-      <div className="stats text-center w-[700px] mx-auto pb-7 pt-2 lg:stats-horizontal stats-vertical shadow">
+      <div className="stats text-center w-[1100px] mx-auto pb-7 lg:stats-horizontal stats-vertical shadow">
         <div className="stat">
-          <div className="stat-title">Downloads</div>
+          <div className="stat-title">Clients</div>
           <div className="stat-value">
             <CountUp
               start={1000}
               end={2347}
               duration={2.75}
-              separator=" "
               decimals={0}
               decimal=","
               onEnd={() => console.log('Ended! 👏')}
@@ -32,19 +31,55 @@ const Stat = () => {
         </div>
 
         <div className="stat">
-          <div className="stat-title">New Users</div>
-          <div className="stat-value">4,200</div>
+          <div className="stat-title">New Tourists</div>
+          <div className="stat-value">
+            <CountUp
+              start={1000}
+              end={4000}
+              duration={2.75}
+              decimals={0}
+              decimal=","
+              onEnd={() => console.log('Ended! 👏')}
+              onStart={() => console.log('Started! 💨')}
+            >
+              {({ countUpRef, start }) => (
+                <div>
+                  <span ref={countUpRef} />
+                  <span>+</span>
+                  <button onClick={start}></button>
+                </div>
+              )}
+            </CountUp>
+          </div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
         <div className="stat">
           <div className="stat-title">New Registers</div>
-          <div className="stat-value">1,200</div>
-          <div className="stat-desc">↘︎ 90 (14%)</div>
+          <div className="stat-value">
+            <CountUp
+              start={800}
+              end={1200}
+              duration={2.75}
+              decimals={0}
+              decimal=","
+              onEnd={() => console.log('Ended! 👏')}
+              onStart={() => console.log('Started! 💨')}
+            >
+              {({ countUpRef, start }) => (
+                <div>
+                  <span ref={countUpRef} />
+                  <span>+</span>
+                  <button onClick={start}></button>
+                </div>
+              )}
+            </CountUp>
+          </div>
+          <div className="stat-desc">↗︎ 90 (14%)</div>
         </div>
 
         <div className="stat text-center mx-auto flex justify-center">
-          <Link to='/rooms'> 
+          <Link to="/rooms">
             <button className="bhutta">
               <span>See Rooms</span>
             </button>
