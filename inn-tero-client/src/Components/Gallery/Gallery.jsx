0cf5a17740908aa5f9e4d8a515a3e08ee7910f24
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-08 19:12:01 +0600

    added gsap pageloader

--- inn-tero-client/src/Components/Gallery/Gallery.jsx
+++ inn-tero-client/src/Components/Gallery/Gallery.jsx
@@ -1,62 +1,66 @@
+import PreLoader from '../Preloader/Preloader';
+
 const Gallery = () => {
   return (
-    <div>
-      <h1 className="text-center font-bold font-script text-8xl mt-20 mb-32">
-        Visual Your{' '}
-        <span className="text-[#aa7474] font-extrabold text-9xl">
-          {' '}
-          Stayings
-        </span>
-      </h1>
+    // <PreLoader>
+      <div>
+        <h1 className="text-center font-bold font-script text-8xl mt-20 mb-32">
+          Visual Your{' '}
+          <span className="text-[#aa7474] font-extrabold text-9xl">
+            {' '}
+            Stayings
+          </span>
+        </h1>
 
-      <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-10">
-        <img
-          className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/QM1nwhk/istockphoto-182686026-170667a.jpg"
-          alt=""
-        />
-        <img
-          className="mt-10 lg:h-[500px] md:h-[300px] rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/ZXKWGhJ/HD-wallpaper-bar-lounge-in-a-skyscraper-skyscraper-lounge-view-bar.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/0X2XQtf/background-music-hotels-lobby.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/LZVx1Tb/HD-wallpaper-hanging-lights-in-bar-setup-lounge.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/TRnjd6r/1000-F-539652332-Z9-M3f-Hz-GJu-Vsu1-ABWv-L0z-J9-UNWTZrq-Nc.jpg "
-          alt=""
-        />
-        <img
-          className=" lg:h-[500px] md:h-[300px] rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/dPzv48X/desktop-wallpaper-hotel-full-page-hotel-reception-interior-hotel-bedroom-interior-outstanding-hote-d.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg lg:h-[500px] md:h-[300px] transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/bHXDPQr/1000-F-539652097-j-Vy-Vv-PJk-VGUBI51-Jj-Rwbm-PFMOv4-BEK3-G.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
-          src="https://i.ibb.co/Pm7BczT/istockphoto-154891054-612x612.jpg"
-          alt=""
-        />
-        <img
-          className="rounded-lg transform scale-100 lg:h-[500px] md:h-[300px] hover:scale-110 transition-transform"
-          src="https://i.ibb.co/g4FbxvL/modern-bedroom-interior-with-neon-lights-glowing-ambient-in-the-evening-ai-generated-photo.jpg"
-          alt=""
-        />
+        <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-10">
+          <img
+            className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/QM1nwhk/istockphoto-182686026-170667a.jpg"
+            alt=""
+          />
+          <img
+            className="mt-10 lg:h-[500px] md:h-[300px] rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/ZXKWGhJ/HD-wallpaper-bar-lounge-in-a-skyscraper-skyscraper-lounge-view-bar.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/0X2XQtf/background-music-hotels-lobby.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/LZVx1Tb/HD-wallpaper-hanging-lights-in-bar-setup-lounge.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/TRnjd6r/1000-F-539652332-Z9-M3f-Hz-GJu-Vsu1-ABWv-L0z-J9-UNWTZrq-Nc.jpg "
+            alt=""
+          />
+          <img
+            className=" lg:h-[500px] md:h-[300px] rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/dPzv48X/desktop-wallpaper-hotel-full-page-hotel-reception-interior-hotel-bedroom-interior-outstanding-hote-d.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg lg:h-[500px] md:h-[300px] transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/bHXDPQr/1000-F-539652097-j-Vy-Vv-PJk-VGUBI51-Jj-Rwbm-PFMOv4-BEK3-G.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg transform scale-100 hover:scale-110 transition-transform"
+            src="https://i.ibb.co/Pm7BczT/istockphoto-154891054-612x612.jpg"
+            alt=""
+          />
+          <img
+            className="rounded-lg transform scale-100 lg:h-[500px] md:h-[300px] hover:scale-110 transition-transform"
+            src="https://i.ibb.co/g4FbxvL/modern-bedroom-interior-with-neon-lights-glowing-ambient-in-the-evening-ai-generated-photo.jpg"
+            alt=""
+          />
+        </div>
       </div>
-    </div>
+    // </PreLoader>
   );
 };
 
--- inn-tero-client/src/Components/Newsletter/Newsletter.jsx
+++ inn-tero-client/src/Components/Newsletter/Newsletter.jsx
@@ -1,6 +1,7 @@
 import { TypeAnimation } from 'react-type-animation';
 import Swal from 'sweetalert2';
 import img from '../../assets/banner.png';
+import PreLoader from '../Preloader/Preloader';
 
 const Newsletter = () => {
   const backBg = {
@@ -35,79 +36,82 @@ const Newsletter = () => {
   };
 
   return (
-    <div>
-      <div style={backBg} className="hero min-h-screen  bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-            <div className="text-center text-red-600 text-3xl mt-20 font-bold">
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed once, initially
-                  'Submit',
-                  1000,
-                  'To',
-                  1000,
-                  'Our',
-                  1000,
-                  'Newsletter',
-                  1000,
-                ]}
-                speed={50}
-                style={{ fontSize: '2em' }}
-                repeat={Infinity}
-              />
-            </div>
-            <form
-              id="deleteSubmit"
-              onSubmit={handleSubmit}
-              className="card-body"
-            >
-              <div className="flex lg:flex-row flex-col md:flex-col gap-10">
+    <>
+      <PreLoader> </PreLoader>
+      <div>
+        <div style={backBg} className="hero min-h-screen  bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+              <div className="text-center text-red-600 text-3xl mt-20 font-bold">
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed once, initially
+                    'Submit',
+                    1000,
+                    'To',
+                    1000,
+                    'Our',
+                    1000,
+                    'Newsletter',
+                    1000,
+                  ]}
+                  speed={50}
+                  style={{ fontSize: '2em' }}
+                  repeat={Infinity}
+                />
+              </div>
+              <form
+                id="deleteSubmit"
+                onSubmit={handleSubmit}
+                className="card-body"
+              >
+                <div className="flex lg:flex-row flex-col md:flex-col gap-10">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      className="input input-bordered"
+                      required
+                      name="name"
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="email"
+                      className="input input-bordered"
+                      required
+                      name="email"
+                    />
+                  </div>
+                </div>
                 <div className="form-control">
                   <label className="label">
-                    <span className="label-text">Name</span>
+                    <span className="label-text">Description</span>
                   </label>
                   <input
                     type="text"
-                    placeholder="Name"
+                    placeholder="Description"
                     className="input input-bordered"
                     required
-                    name="name"
+                    name="description"
                   />
                 </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="email"
-                    placeholder="email"
-                    className="input input-bordered"
-                    required
-                    name="email"
-                  />
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Submit</button>
                 </div>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Description</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Description"
-                  className="input input-bordered"
-                  required
-                  name="description"
-                />
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Submit</button>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
