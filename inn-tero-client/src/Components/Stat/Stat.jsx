f531dcb8723fbec1bfab1e61185115d056c2b74e
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-08 12:06:47 +0600

    added new countup

--- inn-tero-client/src/Components/Stat/Stat.jsx
+++ inn-tero-client/src/Components/Stat/Stat.jsx
@@ -1,15 +1,24 @@
 import '../Styles/statbtn.css';
 import CountUp from 'react-countup';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import ScrollTrigger from 'react-scroll-trigger';
 
 const Stat = () => {
+  const [counterOn, setCounterOn] = useState(false);
+
   return (
-    <div className="flex justify-center">
-      <div className="stats text-center w-[1100px] mx-auto pb-7 lg:stats-horizontal stats-vertical shadow">
-        <div className="stat">
-          <div className="stat-title">Clients</div>
-          <div className="stat-value">
-            <CountUp
+    <>
+      <ScrollTrigger
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <div className="flex justify-center">
+          <div className="stats text-center w-[1100px] mx-auto pb-7 lg:stats-horizontal stats-vertical shadow">
+            <div className="stat">
+              <div className="stat-title">Clients</div>
+              <div className="stat-value">
+                {/* <CountUp
               start={1000}
               end={2347}
               duration={2.75}
@@ -28,17 +37,27 @@ const Stat = () => {
                   <button onClick={start}></button>
                 </div>
               )}
-            </CountUp>
-          </div>
-          <div className="stat-desc">Jan 1st - Feb 1st</div>
-        </div>
+            </CountUp> */}
+                {counterOn && (
+                  <CountUp
+                    start={1000}
+                    end={2347}
+                    duration={2.75}
+                    className="font-script text-orange-400 font-bold mx-auto"
+                    delay={0}
+                  ></CountUp>
+                )}{' '}
+                <span className="font-script">+</span>
+              </div>
+              <div className="stat-desc">Jan 1st - Feb 1st</div>
+            </div>
 
-        <div className="stat">
-          <div className="stat-title">New Tourists</div>
-          <div className="stat-value">
-            <CountUp
+            <div className="stat">
+              <div className="stat-title">New Tourists</div>
+              <div className="stat-value">
+                {/* <CountUp
               start={1000}
-              end={4000}
+              end={2347}
               duration={2.75}
               decimals={0}
               decimal=","
@@ -51,21 +70,31 @@ const Stat = () => {
                     ref={countUpRef}
                     className="font-script text-orange-400 font-bold mx-auto"
                   />
-                  <span className="font-script font-bold mx-auto">+</span>
+                  <span className="font-script">+</span>
                   <button onClick={start}></button>
                 </div>
               )}
-            </CountUp>
-          </div>
-          <div className="stat-desc">↗︎ 400 (22%)</div>
-        </div>
+            </CountUp> */}
+                {counterOn && (
+                  <CountUp
+                    start={3000}
+                    end={4000}
+                    duration={2.75}
+                    className="font-script text-orange-400 font-bold mx-auto"
+                    delay={0}
+                  ></CountUp>
+                )}{' '}
+                <span className="font-script">+</span>
+              </div>
+              <div className="stat-desc">↗︎ 400 (22%)</div>
+            </div>
 
-        <div className="stat">
-          <div className="stat-title">New Registers</div>
-          <div className="stat-value">
-            <CountUp
-              start={800}
-              end={1200}
+            <div className="stat">
+              <div className="stat-title">New Registers</div>
+              <div className="stat-value">
+                {/* <CountUp
+              start={1000}
+              end={2347}
               duration={2.75}
               decimals={0}
               decimal=","
@@ -78,24 +107,36 @@ const Stat = () => {
                     ref={countUpRef}
                     className="font-script text-orange-400 font-bold mx-auto"
                   />
-                  <span className="font-script font-bold mx-auto">+</span>
+                  <span className="font-script">+</span>
                   <button onClick={start}></button>
                 </div>
               )}
-            </CountUp>
-          </div>
-          <div className="stat-desc">↗︎ 90 (14%)</div>
-        </div>
+            </CountUp> */}
+                {counterOn && (
+                  <CountUp
+                    start={2000}
+                    end={3000}
+                    duration={2.75}
+                    className="font-script text-orange-400 font-bold mx-auto"
+                    delay={0}
+                  ></CountUp>
+                )}{' '}
+                <span className="font-script">+</span>
+              </div>
+              <div className="stat-desc">↗︎ 90 (14%)</div>
+            </div>
 
-        <div className="stat text-center mx-auto flex justify-center">
-          <Link to="/rooms">
-            <button className="bhutta">
-              <span>See Rooms</span>
-            </button>
-          </Link>
+            <div className="stat text-center mx-auto flex justify-center">
+              <Link to="/rooms">
+                <button className="bhutta">
+                  <span>See Rooms</span>
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </ScrollTrigger>
+    </>
   );
 };
 
