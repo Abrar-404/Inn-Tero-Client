1430382cc730b08a45a4eb8aae49ed53609b3e23
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-10 22:55:36 +0600

    added designs

--- inn-tero-client/src/Components/TextCircle/TextCircle.jsx
+++ inn-tero-client/src/Components/TextCircle/TextCircle.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import '../Styles/circletext.css';
+import '../Styles/customfont.css';
 
 const TextCircle = () => {
   useEffect(() => {
@@ -16,16 +17,25 @@ const TextCircle = () => {
       console.error("Element with selector '.text p' not found.");
     }
   }, []);
-  // empty dependency array to run the effect only once after initial render
 
   return (
     <div>
-      <div className="flex justify-start gap-10">
+      <div className="flex justify-start md:flex-col lg:flex-row flex-col mx-auto gap-10 border-2 border-red-500">
         <div>
           <img
             src="https://t3.ftcdn.net/jpg/06/19/00/08/360_F_619000872_AxiwLsfQqRHMkNxAbN4l5wg1MsPgBsmo.jpg"
             alt=""
           />
+          <h1 className="text-4xl font-cardo text-[#bdb9b5] font-medium mt-10">
+            BOOK YOUR <br /> DELEGANT <br /> STAYINGS WITH <br /> OUR INN
+          </h1>
+          <div className="border-t-2 border-2 border-black mt-5">{'  '}</div>
+          <h1 className="font-outfit font-bold text-base mt-5">
+            Sahil Abrar,{' '}
+            <span className="text-[#a9774e] text-xs font-thin">
+              Founder & Ceo
+            </span>
+          </h1>
         </div>
 
         <div>
@@ -37,7 +47,7 @@ const TextCircle = () => {
             />
           </div>
 
-          <div className="absolute bottom-[-590px] right-[500px]">
+          <div className="absolute lg:bottom-[-590px] lg:right-[500px] md:right-[470px] md:bottom-[-1500px] bottom-[-1000px] right-[90px]">
             <div className="circle">
               <img
                 className="rounded-full w-[150px] h-[150px]"
@@ -47,6 +57,22 @@ const TextCircle = () => {
               <div className="text">
                 <p>Inn-Tero Experience Elegantly here</p>
               </div>
+            </div>
+          </div>
+          <div className="mt-4">
+            <h1 className="font-cursive font-bold text-3xl text-[#FB9541]">
+              About Our Inn
+            </h1>
+            <h1 className="font-cardo  lg:text-5xl text-3xl">
+              ONE OF THE ELEGANT <br /> INN IN THE AREA
+            </h1>
+            <div>
+              <h1 className="font-outfit text-[#7c8083] mt-5">
+                The masterbuilder of human happiness no one <br /> dislikes, too
+                avoids pleasure itself because it is <br /> pleasure, but
+                because those who do not knows <br /> pleasure rationally
+                encounters consequences <br /> pursues or desires to obtain.
+              </h1>
             </div>
           </div>
         </div>
