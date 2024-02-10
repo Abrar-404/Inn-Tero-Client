6e6fc878d6b9a5ae45b2010fc116db632eefb1bf
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-10 22:08:45 +0600

    added rotating image

--- inn-tero-client/src/Components/TextCircle/TextCircle.jsx
+++ inn-tero-client/src/Components/TextCircle/TextCircle.jsx
@@ -20,9 +20,35 @@ const TextCircle = () => {
 
   return (
     <div>
-      <div className="circle">
-        <div className="text">
-          <p>Inn-Tero Experience Elegantly Withus</p>
+      <div className="flex justify-start gap-10">
+        <div>
+          <img
+            src="https://t3.ftcdn.net/jpg/06/19/00/08/360_F_619000872_AxiwLsfQqRHMkNxAbN4l5wg1MsPgBsmo.jpg"
+            alt=""
+          />
+        </div>
+
+        <div>
+          <div className="relative">
+            <img
+              className="h-[220px] w-[220px]"
+              src="https://i.ibb.co/R0Zq6RY/bedroom-1285156-1280.jpg"
+              alt=""
+            />
+          </div>
+
+          <div className="absolute bottom-[-590px] right-[500px]">
+            <div className="circle">
+              <img
+                className="rounded-full w-[150px] h-[150px]"
+                src="https://media.istockphoto.com/id/1390233984/photo/modern-luxury-bedroom.jpg?s=612x612&w=0&k=20&c=po91poqYoQTbHUpO1LD1HcxCFZVpRG-loAMWZT7YRe4="
+                alt=""
+              />
+              <div className="text">
+                <p>Inn-Tero Experience Elegantly here</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
